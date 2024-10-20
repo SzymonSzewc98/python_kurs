7b2dfcf7daa8ec1779a4ca0519471bcf0cf9de94
Szymon Szewc <sszewc1@sii.pl>
2024-10-20 13:26:16 +0200

    quiz class improvements

--- quiz_system.py
+++ quiz_system.py
@@ -1,6 +1,13 @@
 """
 Main file
 """
+def handle_test_question():
+    quiz = Quiz()
+    quiz.add_question(Question("Jak to jest być skrybą, dobrze, czy niedobrze?",
+                               {"a": "Tak", "b": "Nie", "c": "Nie wiem",
+                                "d": "Nie ma tak, że dobrze albo że nie dobrze"}, "a"))
+    quiz.ask_next_question()
+
 class Question:
     def __init__(self, question, answers, idx_of_true):
         self.question = question
@@ -9,7 +16,8 @@ class Question:
 
     def print_question(self):
         print(self.question, ":")
-        print(self.answers)
+        for answer in self.answers:
+            print(answer, self.answers[answer])
 
     def check_if_is_true(self, idx):
         if idx == self.true_idx:
@@ -29,4 +37,9 @@ class Quiz:
     def ask_next_question(self):
         self.question_list[self.current_question].print_question()
         self.current_question += 1
+
+
+handle_test_question()
+
+
 
