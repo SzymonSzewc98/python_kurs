36baa689f023481f922eac97b82e39248883645a
Szymon Szewc <sszewc1@sii.pl>
2024-10-20 12:58:10 +0200

    Question class added

--- quiz_system.py
+++ quiz_system.py
@@ -1,3 +1,21 @@
 """
 Main file
 """
+class Question:
+    def __init__(self, question, answers, idx_of_true):
+        self.question = question
+        self.answers = answers
+        self.true_idx = idx_of_true
+
+    def print_question(self):
+        print(self.question, ":")
+        print(self.answers)
+
+    def check_if_is_true(self, idx):
+        if idx == self.true_idx:
+            print("Poprawna odpowiedź!")
+            return True
+        else:
+            print("Niepoprawna odpowiedź!")
+            return False
+
